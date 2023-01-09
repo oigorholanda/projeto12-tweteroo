37bbb9f0363fca20ede420b40487d8668e797875
Igor Holanda <oigorholanda@gmail.com>
2023-01-09 19:38:13 -0300

    Display Tweets

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -9,28 +9,39 @@ const users = [];
 const tweets = [];
 const avatars = [];
 
-server.get("/tweets", (req, res) => {
-    res.send(tweets)
-})
+
 
 server.post("/sign-up", (req, res) => {
-    const username = req.body.username
-    const avatar = req.body.avatar
-    users.push(username)
-    avatars.push({username, avatar})
+    const user = req.body
+    
+    users.push(user.username)
+    avatars.push(user)
+
     res.status(201).send("OK")
 })
 
 server.post("/tweets", (req, res) => {
-    const dados = req.body
+    const username = req.body.username
+    const tweet = req.body.tweet
 
-    if (users.includes(dados.username)) {
-        console.log("Usuário Cadastrado")
-        tweets.push(dados)
+    let usersFind = avatars.find(item => item.username === username)
+
+    if (users.includes(username)) {
+        
+        tweets.push({
+            username,
+            avatar: usersFind.avatar, 
+            tweet })
         res.status(201).send("OK")
     } else {
         res.status(401).send('UNAUTHORIZED')
     }
+})
+
+server.get("/tweets", (req, res) => {
+
+
+    res.send(tweets.slice(-10, tweets.length))
 })
 
 
