b2743cf92a2c30ff1ea31b4e8883cf69292bed2a
Igor Holanda <oigorholanda@gmail.com>
2023-01-08 21:47:35 -0300

    Post endpoint

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,18 @@
 import express from 'express'
 
 const server = express()
+server.use(express.json())
+
+const users = [];
 
 server.get("/", (req, res) => {
     res.send("Requisição completa desse endpoint")
+})
+
+server.post("/sign-up", (req, res) => {
+    const dados = req.body
+    users.push(dados.username)
+    res.send(dados.username)
 })
 
 
