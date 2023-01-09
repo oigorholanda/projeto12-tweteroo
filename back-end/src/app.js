471a781c4566ee0be9f2bbac680b6785681b2212
Igor Holanda <oigorholanda@gmail.com>
2023-01-09 16:35:41 -0300

    sing-up created

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,15 +4,28 @@ const server = express()
 server.use(express.json())
 
 const users = [];
+const tweets = [];
 
-server.get("/", (req, res) => {
+server.get("/tweets", (req, res) => {
     res.send("Requisição completa desse endpoint")
 })
 
 server.post("/sign-up", (req, res) => {
     const dados = req.body
     users.push(dados.username)
-    res.send(dados.username)
+    res.status(201).send("OK")
+})
+
+server.post("/tweets", (req, res) => {
+    const dados = req.body
+
+    if (users.includes(dados.username)) {
+        console.log("validado")
+        tweets.push(dados.tweet)
+        res.status(201).send("OK")
+    } else {
+        res.status(401).send('UNAUTHORIZED')
+    }
 })
 
 
